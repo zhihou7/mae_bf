c309e308b50a6d6ca4906a206ff1500ce49b3e38
houzhi <houzhi91@gmail.com>
2022-02-14 10:20:48 +0800

    remove assert for timm

--- main_linprobe.py
+++ main_linprobe.py
@@ -25,7 +25,7 @@ import torchvision.datasets as datasets
 
 import timm
 
-assert timm.__version__ == "0.3.2" # version check
+# assert timm.__version__ == "0.3.2" # version check
 from timm.models.layers import trunc_normal_
 
 import util.misc as misc
